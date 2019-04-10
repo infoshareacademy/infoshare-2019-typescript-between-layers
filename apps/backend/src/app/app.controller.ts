13f756a92f1dc86612c9f256fe7fb3c3c200bed6
Bartosz Cytrowski <bartosz.cytrowski@gmail.com>
2019-04-11 01:12:10 +0200

    WIP: Make nestjs controller

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
@@ -6,8 +6,13 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('todos')
   getData() {
     return this.appService.getData();
+  }
+
+  @Post('addTodo')
+  addTodo() {
+    return this.appService.addTodo();
   }
 }
--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
+interface Todo {
+  id: number,
+  title: string;
+}
+
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return ({ message: 'Welcome to backend!' });
+  todos: Todo[] = [{ id: 1, title: 'Todo 1' }, { id: 2, title: 'Todo 2' }];
+
+  getData(): Todo[] {
+    return this.todos;
+  }
+
+  addTodo() {
+    this.todos.push({
+      id: Date.now(),
+      title: `New todo ${Math.floor(Math.random() * 1000)}`
+    });
   }
 }
